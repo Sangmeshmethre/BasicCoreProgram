20d8383e7d98ca90016f53463d4c987d02dd7e15
Sangmeshmethre <ssmcmethre3080@gmail.com>
2022-09-17 08:24:10 +0530

    CoreProgram

---
+++ BasicProgram195BatchDay4/BasicProgram195BatchDay4/Program.cs
@@ -0,0 +1,46 @@
+﻿using BasicProgram195BatchDay4;
+using System;
+
+namespace BasicProgram195BatchDay5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Core Problems");
+
+            //Console.WriteLine("\t\t Percentage of Head and Tails as :");
+            //FlipCoin.flipCoin();
+
+            //Console.WriteLine("\t\t To Check given Year is Lear Year or Not : ");
+            //LeapYear.leapYear();
+
+            //Console.WriteLine("\t\t Entered Number to print Power Of two ");
+            //Powerof2.powerOf2();
+
+            //Console.WriteLine("\t\t Quotient and Reminder is given as : ");
+            //QuotientNReminder.quotientNreminder();
+
+            //Console.WriteLine("\t\t Swapping of Two Numbers is given as :");
+            //SwapNumbers.swapNumbers();
+
+            //Console.WriteLine("\t\t To get Output as Vowels and Consonant is : ");
+            //VowelsConsonants.vowelsConsonants();
+
+            //Console.WriteLine("\t\t To check given number is Odd or Even as :");
+            //OddEven.oddEven();
+
+            //Console.WriteLine("\t\t Prime Factors of given number are :");
+            //Factors.factors();
+
+            //Console.WriteLine("\t\t The Harmonic vaue of entered number is :");
+            //Harmonic.harmonic();
+
+            Console.WriteLine("\t\t Largest number anong the three numbers is :");
+            LargestNumber.largestNumber();
+
+            Console.ReadLine();
+
+        }
+    }
+}
